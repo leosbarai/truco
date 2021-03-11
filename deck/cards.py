02ccf17518bb7c0adfd724aa73f23d9250b1bcff
Leonardo Sbarai <leonardo.sbarai@br.experian.com>
2021-03-10 22:37:00 -0300

    Criação do método do baralho e de distribuição das cartas.

---
+++ deck/cards.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+class Cards:
+
+    def deck_of_cards_id(self):
+        response = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AD,AC,AH,2S,2D,2C,2H,3S,3D,'
+                                '3C,3H,4S,4D,4C,4H,5S,5D,5C,5H,6S,6D,6C,6H,7S,7D,7C,7H,QS,QD,QC,QH,JS,JD,JC,JH,KS,KD,'
+                                'KC,KH')
+
+        if response.status_code == 200:
+            return response.json()['deck_id']
+        else:
+            print(response.status_code)
+            exit()
+
+    def deliver_cards(self, deck_id, quantity):
+        cards = requests.get('https://deckofcardsapi.com/api/deck/' + str(deck_id) + '/draw/?count=' + str(quantity))
+
+        if cards.status_code == 200:
+            return cards.json()
+        else:
+            print(cards.status_code)
+            exit()
