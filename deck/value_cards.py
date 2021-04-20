437de3e7fda394be4d8ad2fe4ac92833c18d68f4
Leonardo Sbarai <leonardo.sbarai@br.experian.com>
2021-04-20 14:14:59 -0300

    Criação da função para atribuir o valor das cartas.

--- deck/value_cards.py
+++ deck/value_cards.py
@@ -2,57 +2,51 @@ from deck.cards import Cards
 from deck.deliver_cards import DeliverCards
 
 
-class ValueCards:
-    deck_id = Cards().deck_of_cards_id()
-    turned_card = DeliverCards(deck_id).turn_card()
-    player_one_cards = DeliverCards(deck_id).player_cards()
-    player_two_cards = DeliverCards(deck_id).player_cards()
-
-    print(turned_card)
-    print(player_one_cards)
-    print(player_two_cards)
-
-    # for card in p1['cards']:
-    #     card['value'] = cards_value(card.get('value'))
-
-    def cards_value(self, value):
-        switcher = {
-            "QUEEN": 8,
-            "JACK": 9,
-            "KING": 10,
-            "ACE": 11,
-            2: 12,
-            3: 13
-        }
-        return switcher.get(self, value, value)
-
-    def suits_value(self, value):
-        switcher = {
-            "CLUBS": 400,
-            "HEARTS": 300,
-            "SPADES": 200,
-            "DIAMONDS": 100
-        }
-        return switcher.get(value, value)
-
-    def get_manilha(self, value):
-        switcher = {
-            "ACE": 2,
-            2: 3,
-            3: 4,
-            4: 5,
-            5: 6,
-            7: "QUEEN",
-            "QUEEN": "JACK",
-            "JACK": "KING",
-            "KING": "ACE"
-        }
-        return switcher.get(value)
+def assigns_value_cards(cards):
+    switcher = {
+        "QUEEN": 8,
+        "JACK": 9,
+        "KING": 10,
+        "ACE": 11,
+        2: 12,
+        3: 13
+    }
+    return switcher.get(cards, cards)
 
 
-    # def get_value_cards(player_cards):
-    #
-    #     for card in player_cards:
-    #         if card.get('value')
+def suits_value(value):
+    switcher = {
+        'CLUBS': 400,
+        'HEARTS': 300,
+        'SPADES': 200,
+        'DIAMONDS': 100
+    }
+    return switcher.get(value)
 
 
+def get_manilha(value):
+    switcher = {
+        1: 2,
+        2: 3,
+        3: 4,
+        4: 5,
+        5: 6,
+        6: 7,
+        7: 8,
+        8: 9,
+        9: 10,
+        10: 1
+    }
+    return switcher.get(value)
+
+
+def get_value_cards(player_cards, turned_card):
+    manilha = get_manilha(int(turned_card[0].get('value')))
+
+    for card in player_cards:
+        card['value'] = assigns_value_cards(card.get('value'))
+
+        if card['value'] == manilha:
+            card['value'] = suits_value(card.get('suit'))
+
+    return player_cards
