c0e8d414428079f6c3b4115c7226f9288004993d
Leonardo Sbarai <leonardo.sbarai@br.experian.com>
2021-05-25 18:52:40 -0300

    Refatoração do código.

--- api/main/game.py
+++ api/main/game.py
@@ -1,4 +1,3 @@
-from api.deck.value_cards import code_cards
 from api.rules.table import Table
 
 
@@ -13,46 +12,4 @@ class Game:
     table = Table(players_qtt)
     table.players_validate()
     table.players_create()
-
-    while not table.end_game:
-        table.card_distribution()
-        round = 1
-        value = 0
-        while value < table.final_round:
-            print('********** ' + str(round) + 'ª RODADA **********')
-            print('Tombo: ' + table.turned_card[0]['code'])
-
-            for player in table.players:
-                while not player.played:
-                    cards = code_cards(player.cards)
-                    print('Cartas do jogador ' + player.name + ': ' + str(cards))
-                    chosen_card = str(input('Escolha uma carta: ')).upper()
-
-                    while chosen_card not in cards:
-                        print('Cartas do jogador ' + player.name + ': ' + str(cards))
-                        chosen_card = str(input('Carta inválida, escolha novamente: ')).upper()
-
-                    for card in player.cards:
-                        if card['code'] == chosen_card:
-                            player.card_value = int(card['value'])
-                            print('Carta ' + chosen_card + ' na mesa!')
-                            player.cards.remove(card)
-
-                    player.played = True
-
-            round_winner = max(table.players, key=lambda card: card.card_value)
-            round_winner.score += 1
-            print(round_winner.name + ' venceu a ' + str(round) + 'ª rodada.')
-
-            for players in table.players:
-                players.played = False
-
-            value = max(player.score for player in table.players)
-            round += 1
-
-        table.end_game = True
-
-    for players in table.players:
-        if players.score == table.final_round:
-            players.winner_message()
-            table.end_game = True
+    table.play()
--- api/rules/table.py
+++ api/rules/table.py
@@ -1,6 +1,6 @@
 from api.deck.cards import Cards
 from api.deck.deliver_cards import DeliverCards
-from api.deck.value_cards import get_value_cards
+from api.deck.value_cards import get_value_cards, code_cards
 from api.players.players import Players
 
 
@@ -34,3 +34,48 @@ class Table:
         self.turned_card = cards.turn_card()
         for players in self.players:
             players.cards = get_value_cards(cards.player_cards(), self.turned_card)
+
+    def play(self):
+        while not self.end_game:
+            self.card_distribution()
+            round = 1
+            value = 0
+            while value < self.final_round:
+                print('********** ' + str(round) + 'ª RODADA **********')
+                print('Tombo: ' + self.turned_card[0]['code'])
+
+                for player in self.players:
+                    while not player.played:
+                        cards = code_cards(player.cards)
+                        print('Cartas do jogador ' + player.name + ': ' + str(cards))
+                        chosen_card = str(input('Escolha uma carta: ')).upper()
+
+                        while chosen_card not in cards:
+                            print('Cartas do jogador ' + player.name + ': ' + str(cards))
+                            chosen_card = str(input('Carta inválida, escolha novamente: ')).upper()
+
+                        for card in player.cards:
+                            if card['code'] == chosen_card:
+                                player.card_value = int(card['value'])
+                                print('Carta ' + chosen_card + ' na mesa!')
+                                player.cards.remove(card)
+
+                        player.played = True
+
+                round_winner = max(self.players, key=lambda card: card.card_value)
+                round_winner.score += 1
+                print(round_winner.name + ' venceu a ' + str(round) + 'ª rodada.')
+
+                for players in self.players:
+                    players.played = False
+
+                value = max(player.score for player in self.players)
+                round += 1
+
+            self.end_game = True
+            self.winning_player()
+
+    def winning_player(self):
+        for players in self.players:
+            if players.score == self.final_round:
+                players.winner_message()
